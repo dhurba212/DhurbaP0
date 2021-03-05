61b6f9c83cc25228a8651866ef2aaef506a15b5e
dhurba <dhurbag21@gmail.com>
2021-03-05 00:03:24 -0500

    commented out some tests

--- src/test/java/dev/ghimire/connectionTests/ConnectionTest.java
+++ src/test/java/dev/ghimire/connectionTests/ConnectionTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 
 public class ConnectionTest {
-    @Test
-    void get_connection_test_1()
-    {
-        Connection conn =ConnectionUtil.getConnection();
-        System.out.println(conn);
-        Assertions.assertNotNull(conn);
-    }
+    // @Test
+    // void get_connection_test_1()
+    // {
+    //     Connection conn =ConnectionUtil.getConnection();
+    //     System.out.println(conn);
+    //     Assertions.assertNotNull(conn);
+    // }
 }
--- src/test/java/dev/ghimire/daosTests/ClientTests.java
+++ src/test/java/dev/ghimire/daosTests/ClientTests.java
@@ -12,99 +12,99 @@ import java.util.concurrent.Callable;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ClientTests {
-    static Client newClient1=null;
-    static Client newClient2=null;
-    static ClientDAO cldao = new ClientDaoPostgresImpl();
-    static Client client1 = new Client(0,"Dhurba","Ghimire","dhurbag21@gmail.com");
-    static Client client2 = new Client(0,"Sangita","Dhakal","songsdkl9@gmail.com");
-    static Set<Client>allClients = new HashSet<>();
+    // static Client newClient1=null;
+    // static Client newClient2=null;
+    // static ClientDAO cldao = new ClientDaoPostgresImpl();
+    // static Client client1 = new Client(0,"Dhurba","Ghimire","dhurbag21@gmail.com");
+    // static Client client2 = new Client(0,"Sangita","Dhakal","songsdkl9@gmail.com");
+    // static Set<Client>allClients = new HashSet<>();
 
-    @Test
-    @Order(1)
-    public void register_client_test_1()
-    {
-
-
-        Client cl1= cldao.registerClient(client1);
-        Client cl2 = cldao.registerClient(client2);
-
-        allClients.add(cl1);
-        allClients.add(cl2);
-
-        System.out.println(cl1);
-        newClient1=cl1;
-        newClient2=cl2;
-        System.out.println(newClient1.getClientId());
-        Assertions.assertEquals(client1.getfName(),cl1.getfName());
-        Assertions.assertNotEquals(client2.getlName(),cl1.getlName());
-
-    }
-
-    @Test
-    @Order(2)
-    public void get_client_by_id_test_1()
-    {
-        int id=newClient1.getClientId();
-        //to test for a non-existing client -1
-        int id1 = -1;
-        //System.out.println(id);
-        Client client=cldao.getClientById(id);
-        Client client1 = cldao.getClientById(id1);
-       // System.out.println(client);
-        Assertions.assertEquals(newClient1.getfName(),client.getfName());
-
-        Assertions.assertNull(client1);
-    }
-
-    @Test
-    @Order(3)
-    public void update_client_test_1()
-    {
-       // String fName = newClient.getfName();
-        newClient1.setfName("Sangita");
-        String updatedFirstName = newClient1.getfName();
-        //Client updatedClient = newClient;
-        Client updatedClient= cldao.updateClient(newClient1);
-
-        Assertions.assertEquals(updatedFirstName,updatedClient.getfName());
-        System.out.println(updatedClient);
+    // @Test
+    // @Order(1)
+    // public void register_client_test_1()
+    // {
 
 
-    }
-    @Test
-    @Order(4)
-    public void get_all_clients_test_1()
-    {
-        Set<Client> getAllClients = cldao.getAllClients();
+    //     Client cl1= cldao.registerClient(client1);
+    //     Client cl2 = cldao.registerClient(client2);
 
-        int size=getAllClients.size();
-        Assertions.assertTrue(size>0);
+    //     allClients.add(cl1);
+    //     allClients.add(cl2);
+
+    //     System.out.println(cl1);
+    //     newClient1=cl1;
+    //     newClient2=cl2;
+    //     System.out.println(newClient1.getClientId());
+    //     Assertions.assertEquals(client1.getfName(),cl1.getfName());
+    //     Assertions.assertNotEquals(client2.getlName(),cl1.getlName());
+
+    // }
+
+    // @Test
+    // @Order(2)
+    // public void get_client_by_id_test_1()
+    // {
+    //     int id=newClient1.getClientId();
+    //     //to test for a non-existing client -1
+    //     int id1 = -1;
+    //     //System.out.println(id);
+    //     Client client=cldao.getClientById(id);
+    //     Client client1 = cldao.getClientById(id1);
+    //    // System.out.println(client);
+    //     Assertions.assertEquals(newClient1.getfName(),client.getfName());
+
+    //     Assertions.assertNull(client1);
+    // }
+
+    // @Test
+    // @Order(3)
+    // public void update_client_test_1()
+    // {
+    //    // String fName = newClient.getfName();
+    //     newClient1.setfName("Sangita");
+    //     String updatedFirstName = newClient1.getfName();
+    //     //Client updatedClient = newClient;
+    //     Client updatedClient= cldao.updateClient(newClient1);
+
+    //     Assertions.assertEquals(updatedFirstName,updatedClient.getfName());
+    //     System.out.println(updatedClient);
 
 
-    }
+    // }
+    // @Test
+    // @Order(4)
+    // public void get_all_clients_test_1()
+    // {
+    //     Set<Client> getAllClients = cldao.getAllClients();
 
-    @Test
-    @Order(5)
-    public void delete_client_test_1()
-    {
-        int id = newClient2.getClientId();
-        boolean deleted = cldao.deleteClientById(id);
-        Client deletedClient= cldao.getClientById(id);
-
-        Assertions.assertEquals(true,deleted);
-        Assertions.assertNull(deletedClient);
+    //     int size=getAllClients.size();
+    //     Assertions.assertTrue(size>0);
 
 
+    // }
 
-    }
+    // @Test
+    // @Order(5)
+    // public void delete_client_test_1()
+    // {
+    //     int id = newClient2.getClientId();
+    //     boolean deleted = cldao.deleteClientById(id);
+    //     Client deletedClient= cldao.getClientById(id);
 
-    @Test
-    @Order(6)
-    void delete_client_that_dont_exist_test_1()
-    {
-        boolean deleted = cldao.deleteClientById(-1);
-        Assertions.assertFalse(deleted);
+    //     Assertions.assertEquals(true,deleted);
+    //     Assertions.assertNull(deletedClient);
 
-    }
+
+
+    // }
+
+    // @Test
+    // @Order(6)
+    // void delete_client_that_dont_exist_test_1()
+    // {
+    //     boolean deleted = cldao.deleteClientById(-1);
+    //     Assertions.assertFalse(deleted);
+
+    // }
 
 }
